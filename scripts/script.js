08d1c8421ecba779f433fdc6f0b300c48fb39014
Amar Hamlaoui <amarhamlaoui@gmail.com>
2022-02-01 16:52:09 +0100

    Add copyright section and mobile menu.

--- scripts/script.js
+++ scripts/script.js
@@ -93,3 +93,22 @@ button.addEventListener('click', () => {
                             <img src="src/down-arrow.png">`;
     }
 });
+
+function openCloseMenu() {
+    const x = document.querySelector('.mobile-menu');
+    const y = document.querySelector('body');
+    if (x.style.display === 'flex') {
+      x.style.display = 'none';
+      y.style.overflow = 'scroll';
+    } else {
+      x.style.display = 'flex';
+      y.style.overflow = 'hidden';
+    }
+  }
+  function addevent(id) {
+    document.getElementById(id).addEventListener('click', () => {
+      openCloseMenu();
+    });
+  }
+  const ids = ['onclick-open', 'onclick-close', 'onclick-events', 'onclick-about', 'onclick-guests', 'onclick-sponsors'];
+  ids.forEach(addevent);
